4f51b2698ebdd3eda7c8c86db4f30a200f6adb15
Roan Poggere da Silva <371153@unoesc.lan>
2024-10-09 21:35:18 -0300

    tá na mao

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,27 @@
+﻿using BubbleSort;
+
+int[] inteiros = [0, 12, 7, 1, 2, 8];
+
+//Console.WriteLine(inteiros.ToString());
+/*
+Console.Write("[");
+foreach (int i in inteiros){
+    Console.Write(i + " ");
+}
+Console.Write("]");
+*/
+//Mostrar lista desordenada
+BubbleSort.BubbleSort.Print(inteiros);
+
+//Ordenar lista
+BubbleSort.BubbleSort.Sort(inteiros);
+
+//Mostra a lista ordenada
+BubbleSort.BubbleSort.Print(inteiros);
+
+Console.WriteLine("Teste Char");
+
+char[] chars = ['x', 'z', 'b', 'c', 'a'];
+BubbleSort.BubbleSort.Print(chars);
+BubbleSort.BubbleSort.Sort(chars);
+BubbleSort.BubbleSort.Print(chars);
